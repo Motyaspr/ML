8331ec7596cf355c11df19914de0718060cbd729
Matvey Sprikut <sprikutm@gmail.com>
2020-12-26 22:09:34 +0500

    Update H.py

--- CF/H.py
+++ CF/H.py
@@ -1,5 +1,36 @@
-import numpy as np
+import random
 
-g1 = np.array([[1, 0, 1], [1,  1, 0]])
-g2 = np.array([[1, 1, 0, 0], [1, 1, 1, 1], [1, 1, 0, 1]])
-print(np.kron(g1, g2))
+
+m = int(input())
+
+a = list()
+for i in range(2 ** m):
+    x = int(input())
+    if x == 0:
+        continue
+    a.append(i)
+if len(a) == 0:
+    print("1\n1")
+    for i in range(m + 1):
+        print("-1.0", end = ' ')
+    exit(0)
+K = min(len(a), 512)
+print(2)
+print(K, 1)
+for i in range(K):
+    x = a[i]
+    b = 0.5
+    s = ""
+    for j in range(m):
+        if x & (1 << j):
+            s += "1.0 "
+            b -= 1
+        else:
+            s += "-10.0 "
+    s += str(b)
+    print(s)
+for i in range(K + 1):
+    if i == K:
+        print(-0.5, end = ' ')
+    else:
+        print(1.0, end = ' ')
